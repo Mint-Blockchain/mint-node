4ddac7f7d6c765f20a26785e8474883a2a2841ce
Leo <151592921+lyl150@users.noreply.github.com>
2024-11-27 14:13:15 +0800

    Update generate_nginx_config.sh

--- staking/generate_nginx_config.sh
+++ staking/generate_nginx_config.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# 生成 Nginx 配置文件
+
 cat <<EOF > /etc/nginx/nginx.conf
 worker_processes auto;
 
@@ -13,19 +13,19 @@ http {
     default_type application/octet-stream;
     server_tokens off;
 
-    # 定义一个速率限制，每秒最多处理 QPS 请求
+    
     limit_req_zone \$binary_remote_addr zone=ipfs_zone:10m rate=${QPS};
 
     server {
         listen 8088;
 
         location /ipfs {
-            proxy_pass http://ipfs:8080;  # 转发到 IPFS 容器
+            proxy_pass http://ipfs:8080;  
             proxy_set_header Host \$host;
             proxy_set_header X-Real-IP \$remote_addr;
             proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
 
-            # 应用速率限制
+            
             limit_req zone=ipfs_zone burst=10 nodelay;
         }
     }
